c378508c249c566662aa66ae1b21aae3a5a16ad3
Rosey Co Management <roseycomanagement@roseyco.co.uk>
2026-04-08 22:36:38 -0300

    feat: add GA4 tracking (G-1ZYTTJQNDK)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
+import Script from "next/script"
 import "./globals.css"
 import { siteConfig } from "@/config/site"
 
@@ -48,6 +49,15 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         {children}
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-1ZYTTJQNDK" strategy="afterInteractive" />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-1ZYTTJQNDK');
+          `}
+        </Script>
       </body>
     </html>
   )
